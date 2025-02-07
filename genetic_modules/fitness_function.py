379938c47b660d57c717b5c67836ca52096e96c1
nick <nick.notaweeb@gmail.com>
2025-02-07 23:57:03 +0000

    figuring things out, hooking fitness function to genetic system, adding cache from functools to fitness function

--- genetic_modules/fitness_function.py
+++ genetic_modules/fitness_function.py
@@ -1,14 +1,18 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import Tuple
 
 # basic super class, use for polymorphism, multiple methods of calculating fitness
 class FitnessFunction(ABC):
     @abstractmethod
-    def calculate(self, gene: List[bool]) -> float:
+    def calculate(self, gene: Tuple[bool]) -> float:
         pass
 
 
 # inherit super, so this can be used instead
 class DemoFitnessFunction(FitnessFunction):
-    def calculate(self, gene: List[bool]) -> float:
+    def calculate(self, gene: Tuple[bool]) -> float:
         return sum(gene)
+    
+
+# need to find way to cache here
+# https://docs.python.org/dev/library/functools.html#functools.cache
--- genetic_modules/genetic_system.py
+++ genetic_modules/genetic_system.py
@@ -2,8 +2,32 @@ from genetic_modules.genetic_config import GeneticConfig
 from genetic_modules.fitness_function import FitnessFunction
 
 
-# need a way to 'pipe' things in. i.e DataPipe (preprocess data etc), FitnessFunction, SelectionStrategy, GeneticAlgorithms
+# need a way to 'pipe' things in. i.e DataPipe (preprocess data etc), FitnessFunction, SelectionStrategy, GeneticAlgorithms (crossover, mutation, etc. elitism & padding too?)
 class GeneticSystem:
     def __init__(self, config: GeneticConfig, fitness_function: FitnessFunction):
-        self.population = []
-        self.fitness_cache = []
+        self.population = [] # genes must be Tuple[bool]
+
+        self.config = config
+        self.fitness_function = fitness_function
+
+
+        self.elite_threshold = int(config.population_size * config.elite_percent)
+        self.padding_threshold = config.population_size - int(config.population_size * config.padding_percent)
+
+        # pipe data in , preprocess ? turn into genes for pop
+
+
+    
+
+
+
+    def sort_genes_by_fitness(self):
+        self.population.sort(key=lambda g: self.fitness_function.calculate(g), reverse=self.config.maximize_fitness)
+
+
+    def step(self):
+        new_population = self.population[:self.elite_threshold] # copies elite genes over
+
+        
+
+# don't have to re-evaluate genes. fitness_cache must be stored and accessed from multiple places
