4023ceca1809cfb9177d2e4b27ef1cd2675b2081
nick <nick.notaweeb@gmail.com>
2025-02-08 00:07:14 +0000

    fitness now cached ('centralised caching' - kinda cool) and also commented testing stuff in notebook cuz its cool

--- genetic_modules/fitness_function.py
+++ genetic_modules/fitness_function.py
@@ -1,16 +1,23 @@
 from abc import ABC, abstractmethod
 from typing import Tuple
+from functools import cache
 
 # basic super class, use for polymorphism, multiple methods of calculating fitness
 class FitnessFunction(ABC):
-    @abstractmethod
+    # 'centralised' caching - abstracts subclass implementation
+    @cache
     def calculate(self, gene: Tuple[bool]) -> float:
+        return self._calculate(gene)
+
+    # this method will be implemented in subclass
+    @abstractmethod 
+    def _calculate(self, gene: Tuple[bool]) -> float:
         pass
 
 
 # inherit super, so this can be used instead
 class DemoFitnessFunction(FitnessFunction):
-    def calculate(self, gene: Tuple[bool]) -> float:
+    def _calculate(self, gene: Tuple[bool]) -> float:
         return sum(gene)
     
 
