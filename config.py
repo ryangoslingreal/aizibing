981eacce87931a060773aad0901726d3024b2b7c
nick <ngg5@kent.ac.uk>
2025-02-21 14:38:42 +0000

    more config and fitness

--- config.py
+++ config.py
@@ -6,6 +6,10 @@ PARAMS = {
     "SELECTION": selection.roulette_wheel_selection,
     "CROSSOVER": crossover.splice_crossover,
 
+    # GA properties
+    "ELITE_RATE": 0.2,
+    "PADDING_RATE": 0.2,
+    "MUTATION_RATE": 0.01, # maybe implement 'variable mutation' via hamming distance?
 
 
     # Tournament Rounds
