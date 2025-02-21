544e68aabc61deaf82ff0f9de770d877480dfa82
ryangoslingreal <117118805+ryangoslingreal@users.noreply.github.com>
2025-02-21 14:38:13 +0000

    Mutation skeleton

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import selection
 import crossover
+import mutation
 
 PARAMS = {
     # GA methods
     "SELECTION": selection.roulette_wheel_selection,
     "CROSSOVER": crossover.splice_crossover,
-
+    "MUTATION": mutation.random_mutate,
 
 
     # Tournament Rounds
--- mutation.py
+++ mutation.py
@@ -1,9 +1,15 @@
 
-# TODO: Shuffle mutation *
-# TODO: Random mutation
-# TODO: Flip mutation
+def shuffle_mutate(individual):
+    """Randomly shuffles the order of features in the individual."""
+    # TODO
+    pass
 
-def mutate(self, individual, type, mutation_rate):
-    """Mutates an individual with a given mutation rate."""
+def random_mutate(individual, n=1):
+    """Randomly changes `n` random features in the individual."""
+    # TODO
+    pass
+
+def flip_mutate(individual, n=1):
+    """Selects two random features and swaps their states `n` times."""
     # TODO
     pass
