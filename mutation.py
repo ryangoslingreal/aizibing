8e3104b7c9fb064523613eeb5eb907a9be703f37
SaulGuilford <guilford.saul@gmail.com>
2025-02-23 11:25:19 +0000

    basic code for mutation including shuffle_mutate, random_mutate and flip_mutate.

--- mutation.py
+++ mutation.py
@@ -1,15 +1,35 @@
+import random
 
 def shuffle_mutate(individual):
     """Randomly shuffles the order of features in the individual."""
-    # TODO
-    pass
+    ind = individual 
+    random.shuffle(ind)
+    return ind
+
 
 def random_mutate(individual, n=1):
+    #When mutation is called do we always want it to mutate or just have a chance to mutate?
     """Randomly changes `n` random features in the individual."""
-    # TODO
-    pass
+    mutationRate = 1/len(individual)
+    ind = individual
+    for attribute in individual:
+        chance_of_mutation = random.random()
+        if (chance_of_mutation < mutationRate):
+            ind[attribute] = not(attribute)
+        
+    return ind
 
 def flip_mutate(individual, n=1):
     """Selects two random features and swaps their states `n` times."""
-    # TODO
-    pass
+    attribute_1 = round(random.random()*(len(individual)-1))
+    attribute_2 = round(random.random()*(len(individual)-1))
+    while(attribute_2==attribute_1):
+        attribute_2 = round(random.random()*(len(individual)-1))
+
+    for i in range(n):
+        one = individual[attribute_1]
+        two = individual[attribute_2]
+        individual[attribute_1] = two
+        individual[attribute_2] = one
+        
+    return individual
