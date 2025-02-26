cd650d6c06d3b4cefc8b784baf3b2757672ea32a
albert <albertchenn@live.com>
2025-02-26 22:05:25 +0000

    fixing still: evolve_generation isnt keeping the elites but is changing according to SOME logic not sure if its running as intended otherwise

--- genocide.py
+++ genocide.py
@@ -2,7 +2,8 @@ from config import *
 
 def elitism(sorted_population, elite_rate):
     pre_kill = sorted_population
-    elite_ratio = round(len(pre_kill) * elite_rate)
+    elite_ratio = max(2,round(len(pre_kill) * elite_rate))
+    print(f"elite_ratio = {elite_ratio}")
     after_kill = pre_kill[:elite_ratio]
 
     return after_kill
