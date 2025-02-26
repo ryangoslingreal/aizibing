5be80ac33844bf3e68f5e4cc61513a4ee37f537a
albert <albertchenn@live.com>
2025-02-26 17:09:50 +0000

    DEBUGGING

--- genocide.py
+++ genocide.py
@@ -2,7 +2,7 @@ from config import params
 
 def elitism(sorted_population):
     pre_kill = sorted_population
-    elite_ratio = round(len(pre_kill) * params.ELITE_RATIO)
+    elite_ratio = round(len(pre_kill) * params.ELITE_RATE)
     after_kill = pre_kill[:elite_ratio]
 
     return after_kill
