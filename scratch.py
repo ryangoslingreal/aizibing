2c82f21bb5a80baf3dac9ea62dfde4e7781dca03
albert <albertchenn@live.com>
2025-02-26 17:04:27 +0000

    Revert "average_crossover is now debugged."
    This reverts commit 9fe308fd9a64fddf4e23f7f18be2f6a244479890.

--- scratch.py
+++ scratch.py
@@ -1,24 +1,14 @@
 # crossover.py DEBUGGER
+# splice_crossover
 from random import randint, choice
-from crossover import *
 
-rand_size = randint(5, 10)  # Ensure at least one element
+rand_size = randint(5, 100)  # Ensure at least one element
 fake_parent1 = [choice([0, 1]) for _ in range(rand_size)]
 fake_parent2 = [choice([0, 1]) for _ in range(rand_size)]
+
 print(f'parent1 = {fake_parent1}')
 print(f'parent2 = {fake_parent2}')
-# -----------------------------------------------------------------------------------------
-# splice_crossover
-# to see the information for index, splice size, etc... : need to change function to return what you need
 
-
-# children = splice_crossover(fake_parent1, fake_parent2)
-# print(f'children = {children}')
-
-# -----------------------------------------------------------------------------------------
-# average_crossover
-
-child = average_crossover(fake_parent1, fake_parent2)
-print(child)
-
-# -----------------------------------------------------------------------------------------
+from crossover import *
+children = splice_crossover(fake_parent1, fake_parent2)
+print(f'children = {children}')
