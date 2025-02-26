ab26b80fc60498db9fc31fc3658ea8cb472f0a17
albert <albertchenn@live.com>
2025-02-26 21:33:59 +0000

    ga.py actually runs now.
    every generation changes but the top score changes aswell( needs debugging)

--- ga.py
+++ ga.py
@@ -39,15 +39,16 @@ class GeneticAlgorithm():
             print(f"Position {i}: {individual}    Fitness: {fitness}")
 
     def evolve_generation(self):
-        self.population = params.KILL_METHOD(self.population)
+        self.population = params.KILL_METHOD(self.population, params.ELITE_RATE)
         difference = params.POPULATION - len(self.population)
 
         if difference > 0:
-            padding_amount = round(len(params.POPULATION * params.PADDING_RATE))
-            parent1, parent2 = random.choice(self.population), random.choice(self.population) # randomly pick 2 parents from killed population
+            padding_amount = round(params.POPULATION * params.PADDING_RATE)
 
             for an_individual in range(difference):
-                self.population += params.CROSSOVER(parent1, parent2)[random.choice]# picks 1 child in case a method returns multiple
+                parent1, parent2 = random.choice(self.population), random.choice(
+                    self.population)  # randomly pick 2 parents from killed population
+                self.population += params.CROSSOVER(parent1, parent2)# picks 1 child in case a method returns multiple
 
             self.population = self.population[-1:padding_amount]
 
