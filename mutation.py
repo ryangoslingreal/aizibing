f26c897a3ba3d78705a93e8f23c5f869e8100ede
SaulGuilford <guilford.saul@gmail.com>
2025-03-26 05:42:41 +0000

    Edited all mutator methods to guarantee mutation and corrected previous commit. the required mutators will not mutate the individual in the event that all values  are true, or if all values are false, the verifyIndividual method will mutate the individual.

--- mutation.py
+++ mutation.py
@@ -4,29 +4,36 @@ import random
 
 def shuffle_mutate(individual):
     """Randomly shuffles the order of features in the individual."""
-    
-    shuffled_individual = random.shuffle(individual)
 
-    while(shuffled_individual == individual):
-        shuffled_individual = random.shuffle(individual)
+    non_mutated_individual = individual[:]
 
-    return verifyIndividual(shuffled_individual)
+    while(non_mutated_individual == individual and not all(individual) and not any(individual)):
+        random.shuffle(individual)
+
+    return verifyIndividual(individual)
 
 
 def random_mutate(individual, n=1):
     """Randomly changes `n` random features in the individual."""
-    gene_indices =  np.random.choice(len(individual), size=n, replace=False)
+
+    non_mutated_individual = individual[:]
+
+    while(non_mutated_individual == individual):
+        gene_indices =  np.random.choice(len(individual), size=n, replace=False)
     
-    for gene_index in gene_indices:
-        individual[gene_index] = not(individual[gene_index])
-        
+        for gene_index in gene_indices:
+            individual[gene_index] = not(individual[gene_index])
+            
     return verifyIndividual(individual)
 
 def flip_mutate(individual, n=1):
     """Selects two random features and swaps their states `n` times."""
     
-    for _ in range(n):
-        gene1, gene2 =  np.random.choice(len(individual), size=2, replace=False)
-        individual[gene1], individual[gene2] = individual[gene2], individual[gene1]
-        
+    non_mutated_individual = individual[:]
+
+    while (non_mutated_individual == individual and not all(individual) and any(individual)):
+        for _ in range(n):
+            gene1, gene2 =  np.random.choice(len(individual), size=2, replace=False)
+            individual[gene1], individual[gene2] = individual[gene2], individual[gene1]
+            
     return verifyIndividual(individual)
